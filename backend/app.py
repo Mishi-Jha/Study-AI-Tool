38c3176d749fbd2be86e025b7cefcf8cae6cca7b
Mishi Jha <mishijha22@gmail.com>
2026-07-01 01:53:23 +0530

    feat: add flask server with /chat endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from groq import Groq
 import os
 from dotenv import load_dotenv
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+app = Flask(__name__)
+CORS(app)
 system_prompt={"role":"system","content":"You are a CS Study tool so answer all queries in reference to computer science as the root.Explain things clearly not leaving any threads open. Give prerequisite knowledge as well as necessary information like time complexity and optimal approach.After every explanation, ask the student ONE follow-up question to check understanding. If a topic is important for interviews, mention it"}
 query_list=[system_prompt]
-while True:
-    user_input=input("Write a message..")
-    if user_input=="quit":
-        break
-    query_list.append({"role": "user", "content": user_input})
+@app.route("/chat",methods=["POST"])
+def chat():
+    data=request.json
+    user_message=data.get("message")
+    query_list.append({"role":"user","content":user_message})
     response=client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=query_list
     )
-    print(response.choices[0].message.content)
-    query_list.append({"role": "assistant", "content": response.choices[0].message.content})
+    reply=response.choices[0].message.content
+    query_list.append({"role":"assistant","content":reply})
+    return jsonify({"reply":reply})
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
