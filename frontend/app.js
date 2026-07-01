2e13f11e80db25002207d235e7533eaf1457264b
Mishi Jha <mishijha22@gmail.com>
2026-07-02 03:31:30 +0530

    feat: improve chat UX- instant message display & enter key support

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,19 @@
 const btn=document.getElementById("send-btn");
 const userInput=document.getElementById("user-input");
+userInput.addEventListener("keydown",function(event){
+    if(event.key==="Enter" && !event.shiftKey){
+        event.preventDefault();
+        btn.click();
+    }
+})
 btn.addEventListener("click",async function(event){
     const message=userInput.value;
+    userInput.value="";
+    const chatdiv=document.getElementById("chat-window");
+    const query=document.createElement("p");
+    query.textContent=message;
+    chatdiv.appendChild(query);
+    
     try{
         const res=await fetch("http://127.0.0.1:5000/chat",{
             method:"POST",
@@ -11,11 +23,11 @@ btn.addEventListener("click",async function(event){
             body:JSON.stringify({message: message})
         })
         const data=await res.json();
-        const chatdiv=document.getElementById("chat-window");
+        
         const reply=document.createElement("p");
         reply.textContent=data.reply;
         chatdiv.appendChild(reply);
-
+        
     }catch(err){
         console.log(err);
     }
